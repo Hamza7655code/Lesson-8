e365f3a43ad322d113d2adb0372fd9649637a290
Hamza7655code <mughis.rehman@yahoo.com>
2025-03-06 17:28:58 +0000

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+c = 7
+d = 12
+e = (a / b) * ( c + d ) ** b // a
+print("The value of the expression (a / b) * ( c + d ) ** b // a is ", e)
+
+name = "Mathhew"
+age = 13
+if name == "Mathhew" and age >= 13:
+    print("You are my best friend")
+else:
+    print("I don't know you, that is why you aren't invited to my birthday party")
+
