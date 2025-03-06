a2baa11210307d30a9685fcafcd90b8866fa1087
Hamza7655code <mughis.rehman@yahoo.com>
2025-03-06 17:37:37 +0000

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,6 @@
+numn = int(input("Enter in the numerator of your number"))
+numd = int(input("Enter in the denominator of your number"))
+if numn % numd == 0:
+    print("You have a number with no remainders")
+else:
+    print("your number is not divisible")
